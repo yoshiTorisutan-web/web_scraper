e8a30af6609ff40939040895f3522e216f143a62
yoshiTorisutan-web <94896440+yoshiTorisutan-web@users.noreply.github.com>
2025-12-07 15:53:30 +0100

    Add title to the document head

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Web Content Scraper</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
